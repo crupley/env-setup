7443b13bcaf9954d7fc2274717e530903efd8e97
Chris Rupley <crupley@salesforce.com>
2018-12-11 17:16:10 -0800

    add some installations

--- setup.sh
+++ setup.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
 # Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew doctor
+
+brew tap caskroom/versions
+brew tap caskroom/cask
 
 # Brew Apps
 # iTerm2, atom, open java, jq, awscli, gradle, maven, scala, spark, avro-tools, ammonite
+brew cask install iterm2
 
 # Other
 # chrome, dropbox, 1password, spotify, slack, alfred, todoist, postman, spectacle, rescuetime, intellij ce, quip
-
+brew cask install google-chrome
+brew cask install dropbox
 
 # Anaconda
